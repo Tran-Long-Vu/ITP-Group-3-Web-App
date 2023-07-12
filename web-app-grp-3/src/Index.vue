a49536e2084e99af2bb499824688af52c02b560b
103426463 <tranlongvu007@gmail.com>
2023-07-12 08:45:58 +0700

    first commit

---
+++ web-app-grp-3/src/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <Strengths />
+    </div>
+  </template>
+  
+  <script>
+  import Strengths from './components/Strengths.vue';
+  
+  export default {
+    name: 'Index',
+    components: {
+      Strengths,
+    },
+  };
+  </script>
