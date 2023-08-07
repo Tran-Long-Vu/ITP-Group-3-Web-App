fb1483f26b16ae4dc50e5ef01ced4e8d3dcee0e8
dasfeil <sfbgamng@gmail.com>
2023-08-08 06:16:05 +0700

    Finished calendar

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 // CORS middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -37,7 +37,7 @@ app.post('/login', (req, res) => {
       console.log(err);
       res.status(500).json({ message: 'Internal server error' });
     }
-    if (row) { 
+    if (row) {
       console.log(row);
       // Successful login
       res.status(200).json({ message: 'Login successful', userData: row });
@@ -48,25 +48,34 @@ app.post('/login', (req, res) => {
   });
 });
 
-app.post('/calendar', (req, res) => {
-  const { email, password } = req.body;
-
-  // Query the database to check if the email and password match
-  db.get('SELECT * FROM USERS WHERE email = ? AND password = ?', [email, password], (err, row) => {
-    if (err) {
-      console.log(err);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-    if (row) { 
-      console.log(row);
-      // Successful login
-      res.status(200).json({ message: 'Login successful', userData: row });
-    } else {
-      // Invalid email or password
-      res.status(401).json({ message: 'Invalid email or password' });
-    }
-  });
+app.get('/calendars/:data', (req, res) => {
+  if (req.params.data == "users") {
+    db.all('SELECT UID, name, attendance_count, absence_count FROM USERS', (err, data) => {
+      if (err) {
+        console.log(err)
+        res.status(500).json({ message: 'Internal server error' })
+      }
+      if (data) {
+        res.send({ data })
+      } else {
+        res.status(204).json({ message: 'No user in database' })
+      }
+    })
+  } else if (req.params.data == "attendance") {
+    db.all('SELECT * FROM ATTENDANCE', (err, data) => {
+      if (err) {
+        console.log(err)
+        res.status(500).json({ message: 'Internal server error' })
+      }
+      if (data) {
+        res.send({ data })
+      } else {
+        res.status(204).json({ message: 'No attendance in database' })
+      }
+    })
+  }
 });
+
 
 // Redirect all other routes to the Vue.js app
 app.get('*', (req, res) => {
